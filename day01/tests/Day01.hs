ad111fe5aeac9ee4110d010fceae8e44ad822c69
Jairinho <t4ngz3r0@gmail.com>
2020-05-28 13:53:46 -0400

    handling the test results

--- day01/tests/Day01.hs
+++ day01/tests/Day01.hs
@@ -5,7 +5,9 @@ module Main
 import           Day01.PartOne
 import           Day01.PartTwo
 import           System.Environment
+import           System.Exit
 import           Test.HUnit
+import           Test.HUnit.Base
 
 run = flip fmap $ readFile "input.txt"
 
@@ -21,4 +23,9 @@ partTwo =
     answer <- run Day01.PartTwo.solve
     assertEqual "the right answer is 5043167" 5043167 (read answer)
 
-main = runTestTT $ TestList [partOne, partTwo]
+main = do
+  counts <- runTestTT $ TestList [partOne, partTwo]
+  finish $ (errors counts) + (failures counts)
+  where
+    finish 0 = exitSuccess
+    finish _ = exitFailure
