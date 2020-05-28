3c7e8e1fbb1c2810ad38a87d5f28ecefbbf07202
Jairinho <t4ngz3r0@gmail.com>
2020-05-28 16:18:47 -0400

    refactor on day 02

--- day02/src/Day02/PartOne.hs
+++ day02/src/Day02/PartOne.hs
@@ -7,18 +7,19 @@ import           Data.List.Split
 solve :: String -> Int
 solve = head . process 0 . program
   where
-    program = init . map read . splitOn ","
-    init (x:_:_:xs) = x : [12, 2] ++ xs
+    program = setup . map read . splitOn ","
+    setup (x:_:_:xs) = x : [12, 2] ++ xs
 
 process :: Int -> [Int] -> [Int]
-process pc prg
-  | code == 1 = next $ sum
-  | code == 2 = next $ mul
-  | code == 99 = prg
+process pc program
+  | code == 1 = next $ exec (+)
+  | code == 2 = next $ exec (*)
+  | code == 99 = program
   where
-    code = prg !! pc
+    code = program !! pc
     next = process $ pc + 4
-    sum = set prg (prg !! (pc + 3)) $ (value prg 1) + (value prg 2)
-    mul = set prg (prg !! (pc + 3)) $ (value prg 1) * (value prg 2)
-    value p o = (p !! (p !! (pc + o)))
-    set l i v = (take i l) ++ (v : (snd $ splitAt (i + 1) l))
+    exec op = set program result $ op (value program 1) (value program 2)
+      where
+        result = (program !! (pc + 3))
+        value p o = (p !! (p !! (pc + o)))
+        set l i v = (take i l) ++ v : (snd $ splitAt (i + 1) l)
